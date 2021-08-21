8147b6a622fda561c90c49458e8d511d1b929d5b
andrescrd <c.andresricardo@gmail.com>
2021-08-20 22:58:45 -0300

    test

--- src/components/layout/DashboardSidebar.tsx
+++ src/components/layout/DashboardSidebar.tsx
@@ -69,11 +69,11 @@ const items = [
   //   icon: UserPlusIcon,
   //   title: 'Register'
   // },
-  // {
-  //   href: '/404',
-  //   icon: AlertCircleIcon,
-  //   title: 'Error'
-  // }
+  {
+    href: '/404',
+    icon: AlertCircleIcon,
+    title: 'Error'
+  }
 ];
 
 const DashboardSidebar: React.FC<any> = ({ onMobileClose, openMobile }) => {
